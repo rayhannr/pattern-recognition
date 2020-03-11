8898da1371d0240f5dc2b0e7457729916cc0cee0
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2020-03-11 22:49:44 +0700

    ganti nama variable di project

--- Text Processing/project/similarity.py
+++ Text Processing/project/similarity.py
@@ -8,27 +8,27 @@ from nltk.tokenize import word_tokenize
 
 corpus = [] #array containing words inside each abstract
 for i in range(2):
-    input_file = open("abstrak{}.txt".format(i+1), "r")
-    abstract = []
+    abstract_file = open("abstrak{}.txt".format(i+1), "r")
+    abstract_words = []
     stopwords = StopWordRemoverFactory().create_stop_word_remover()
     stemmer = StemmerFactory().create_stemmer()
     
     #extracting words in every line of the selected abstract
-    for text in input_file:
-        if text.strip():
-            tokens = sent_tokenize(text) #making array of sentences
-            for token in tokens:
-                token = token.replace('\n', '').lower() #hapus newline dan buat lowercase
-                token = re.sub(r'\d+','', token) #hapus angka
-                token = token.translate(token.maketrans('','',string.punctuation)) #hapus tanda baca
-                token = stopwords.remove(token) #remove stopwords
-                token = stemmer.stem(token) #stem kata ke mashdarnya
-                token = word_tokenize(token) #split kalimat jadi tiap array of words
-                abstract.append(token)           
-    input_file.close()
+    for line in abstract_file:
+        if line.strip():
+            sentence = sent_tokenize(line) #making array of sentences
+            for word in sentence:
+                word = word.replace('\n', '').lower() #hapus newline dan buat lowercase
+                word = re.sub(r'\d+','', word) #hapus angka
+                word = word.translate(word.maketrans('','',string.punctuation)) #hapus tanda baca
+                word = stopwords.remove(word) #remove stopwords
+                word = stemmer.stem(word) #stem kata ke mashdarnya
+                word = word_tokenize(word) #split kalimat jadi tiap array of words
+                abstract_words.append(word)           
+    abstract_file.close()
     
     #combining line of words in abstract into one, then adding it into corpus
     combined_line = []
-    for line in abstract:
+    for line in abstract_words:
         combined_line += line
     corpus.append(combined_line)
